a0e7ef82eb54b3ca83ef9909885956f5bb6779f0
mbklementowski@gmail.com <mbklementowski@gmail.com>
2022-03-27 15:18:45 +0200

    problem z komorkami bez sasiadow

--- Saper2/Saper.cpp
+++ Saper2/Saper.cpp
@@ -14,176 +14,211 @@ public:
 	bool isFlag = false;
 	bool isNumber = false;
 	int id;
-	int neighbour{};
-	std::string sprit; 
+	
+	std::string sprit = " O ";
 	std::string flag = "(F)";
 
 	Cell(int x) : id(x){
-		sprit = "(?)";
-		if (rand() % 4 == 1) {
+		
+		if (rand() % 7 == 1) {
 			isBomb = 1;
-			sprit = "(B)";
+			sprit = " B ";
 		}
-		
-		
 	}
 
 	void showID(){
 		std::cout << id << std::endl;
 	}
+
 	int checkNeighbour() {
 		if (isBomb == 1) {
 			return 1;
 		}
 	}
+
 	void checkBomb() {
 		if (move < 1) {
-			isBomb = 0;
+			if (isBomb == 1) {
+				isBomb = 0;
+				sprit = "(?)";
+			}
 		}
-		if (isBomb == 1) {
+		else if (isBomb == 1) {
 			std::cout << "GAME OVER";
 			gameOver = 1;
 		}
 	}
+
 	void changeSprite(int x){
 		std::string s = std::to_string(x);
 		sprit = '(' + s + ')';
 		
 	}
 
-	
-
+	void useFlag() {
+		sprit = flag;
+	}
 };
 
 class Board {
-	std::vector<Cell*> field;
+	std::vector<std::vector<Cell*>> field;
 	int size;
 public:
 	Board(int x) : size(x){
-		for (int i = 0; i < (size*size); i++) {
-			field.push_back(new Cell(i));
+		for (int j = 0; j < size; j++) {
+			std::vector<Cell*> temp;
+				for (int i = 0; i < (size); i++) {
+					temp.push_back(new Cell(i));
+			}
+				field.push_back(temp);
+		}
 	}
-	}
-	bool isValid(int x)
+
+	bool isValid(int x, int y)
 	{
-		return (x >= 0) && (x <= (size*size));
+		return (x >= 0) && (x < size) && (y >= 0) && (y < size);
 	}
-
-	void selectCoord(int x, int y) {
-		int count{};
-		int algo= (y - 1) * size + (x - 1);
-		Cell* ptr = field.at(algo);
-		ptr->checkBomb();
-		
-		Cell* n = nullptr;
-		algo = (y - 0) * size + (x - 2);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y - 0) * size + (x - 1);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-		
-		algo = (y - 0) * size + (x + 0);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y - 1) * size + (x - 2);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y - 1) * size + (x - 0);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y + 1) * size + (x - 2);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y + 1) * size + (x - 1);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y + 1) * size + (x - 0);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-
-		algo = (y - 1) * size + (x - 1);
-		if (isValid(algo) == 1) {
-			n = field.at(algo);
-			if (n->checkNeighbour() == 1) {
-				count++;
-			}
-		}
-		
-		if (count > 0) {
-			ptr->changeSprite(count);
-		}
 	
+	void selectCoord(int a, int b) {
+		
+		int count{};
+		int y = a - 1;
+		int x = b - 1;
+
+		if(isValid(x, y) == 1) {
+
+			Cell* ptr = field.at(x).at(y);
+			ptr->checkBomb();
+
+			Cell* n = nullptr;
+
+			if (isValid((x - 1), y) == 1) {
+				Cell* n = field.at(x - 1).at(y);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid((x + 1), y) == 1) {
+				Cell* n = field.at(x + 1).at(y);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid((x - 1), (y + 1)) == 1) {
+				Cell* n = field.at(x - 1).at(y + 1);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid(x, (y + 1)) == 1) {
+				Cell* n = field.at(x).at(y + 1);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid((x + 1), (y + 1)) == 1) {
+				Cell* n = field.at(x + 1).at(y + 1);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid((x - 1), (y - 1)) == 1) {
+				Cell* n = field.at(x - 1).at(y - 1);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid(x, (y - 1)) == 1) {
+				Cell* n = field.at(x).at(y - 1);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+			if (isValid((x + 1), (y - 1)) == 1) {
+				Cell* n = field.at(x + 1).at(y - 1);
+				if (n->checkNeighbour() == 1) {
+					count++;
+				}
+			}
+
+
+
+			if (count > 0) {
+				ptr->changeSprite(count);
+			}
+			n = nullptr;
+			ptr = nullptr;
+		}
+			
+			if(count < 1) {
+				Cell* p = nullptr;
+				if (isValid((a + 1), b) == 1)
+					selectCoord((a + 1), b);
+				if (isValid((a - 1), b) == 1)
+					selectCoord((a - 1), b);
+				if (isValid((a + 1), (b + 1)) == 1)
+					selectCoord((a + 1), (b + 1));
+				if (isValid(a, (b + 1)) == 1)
+					selectCoord(a, (b + 1));
+				if (isValid((a - 1), (b + 1)) == 1)
+					selectCoord((a - 1), (b + 1));
+				if (isValid((a + 1), (b - 1)) == 1)
+					selectCoord((a + 1), (b - 1));
+				if (isValid(a, (b - 1)) == 1)
+					selectCoord(a, (b - 1));
+				if (isValid((a - 1), (b - 1)) == 1)
+					selectCoord((a - 1), (b - 1));
+			}
+			*/
+		
 	}
+
+	
 	
 	void showBoard() {
-		
-		for (int i = 0; i < (size*size); i++) {
-			if (i != 0 && i != 1 && i % size == 0) {
-				std::cout << " " << std::endl;
+
+		for (int x = 0; x < field.size(); x++)  
+		{
+			for (int y = 0; y < field.size(); y++)  
+			{
+				std::cout << field[x][y] -> sprit;  
 			}
-				std::cout << field[i]->sprit << ' ';
+			std::cout << std::endl;
 		}
+		
 	}
 };
 
-int main() {
-	
-		int x, y;
-		int size;
-		std::cout << "Wybierz wielkosc planszy, wpisujac liczbe od 0-16" << std::endl;
-		std::cin >> size;
-		Board b(size);
-		do {
-		b.showBoard();
-		std::cout << " " << std::endl;
-		std::cout << "Wybierz koordynaty" << std::endl;
-		std::cin >> x;
-		std::cin >> y;
-		b.selectCoord(x, y);
-		move++;
+	int main() {
+		
+			int x, y;
+			int size;
+			
+			std::cout << "Wybierz wielkosc planszy, wpisujac liczbe od 0-16" << std::endl;
+			std::cin >> size;
+			Board b(size);
+			do {
+				b.showBoard();
+				std::cout << " " << std::endl;
+				std::cout << "Wybierz koordynaty wpisujac liczbe od 1 do " << size << std::endl;
+				std::cout << "Szerokosc: " << std::endl;
+				std::cin >> x;
+				std::cout << " " << std::endl;
+				std::cout << "Wysokosc: " << std::endl;
+				std::cin >> y;
+				std::cout << " " << std::endl;
+				b.selectCoord(x, y);
+				move++;
+				
+
+			} while (gameOver == false);
 		
 
-	}
-	while(gameOver == false);
-	
-
-};
+	};
